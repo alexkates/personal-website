c23f98eae20982730a0d758b72b8336595c18691
Daniel Alexander Kates <alex.kates@creditly.com>
2021-05-17 16:46:03 -0400

    added social links

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDev, faGithub, faStackOverflow, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import './Header.css';
 
 export default function Header() {
   return (
     <header>
       <a href="/" title="Senior full-stack developer and solutions architect">Senior full-stack developer and solutions architect</a>
+      <div>
+        <a href="https://github.com/alexkates" target="blank">
+          <FontAwesomeIcon icon={faGithub} className="social-icon" />
+        </a>
+        <a href="https://stackoverflow.com/users/3149835/alex?tab=profile" target="blank">
+          <FontAwesomeIcon icon={faStackOverflow} className="social-icon" />
+        </a>
+        <a href="https://twitter.com/thealexkates" target="blank">
+          <FontAwesomeIcon icon={faTwitter} className="social-icon" />
+        </a>
+        <a href="https://dev.to/thealexkates" target="blank">
+          <FontAwesomeIcon icon={faDev} className="social-icon" />
+        </a>
+      </div>
     </header>
   );
 }
