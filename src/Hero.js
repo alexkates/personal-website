9673b214061e7316bb74971fa9d1defd7275ab33
Daniel Alexander Kates <alex.kates@creditly.com>
2021-05-27 07:36:44 -0400

    minor style tweaks

--- src/Hero.js
+++ src/Hero.js
@@ -3,18 +3,14 @@ export default function Hero() {
     return (
         <section className="mt-40">
             <h1 className="text-6xl">Hi, I'm Alex.</h1>
-            <p className="text-4xl mt-8 opacity-70">
-                Senior Software Engineer @<a className="underline hover:text-gray-300" href="https://creditgenie.com">Credit Genie</a>.
+            <p className="text-4xl mt-8">
+                Senior Software Engineer @ <a className="hover:opacity-90 hover:underline" href="https://creditgenie.com">https://creditgenie.com</a>
             </p>
-            <p className="text-2xl mt-8 opacity-70">
-                AWS Certified Solutions Architect and Community Builder.
-            </p>
-            <p className="text-2xl mt-8 opacity-70">
-                Passionate about full-stack JavaScript, AWS Serverless, and Extreme Programming.
-            </p>
-            <p className="text-2xl mt-8 opacity-70">
-                I also enjoy rock climbing, hiking, cycling, video games, and spending time with my family in my free time.
-            </p>
+            <div className="text-2xl mt-8 opacity-70">
+                <p>AWS Certified Solutions Architect and Community Builder.</p>
+                <p>Passionate about full-stack JavaScript, AWS Serverless, and Extreme Programming.</p>
+                <p>I also enjoy rock climbing, hiking, cycling, video games, and spending time with my family in my free time.</p>
+            </div>
         </section>
     );
 }
