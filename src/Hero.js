5d1839a4abca280f40ef610ff858f490fa2e77b1
Daniel Alexander Kates <alex.kates@creditly.com>
2021-05-30 20:01:51 -0400

    added me_waving on large displays

--- src/Hero.js
+++ src/Hero.js
@@ -1,23 +1,27 @@
 
 import Typewriter from "typewriter-effect";
+import me from './me_waving.png';
 
 export default function Hero() {
     return (
-        <section className="mt-12 md:mt-24">
-            <h1 className="text-center lg:text-left text-6xl">Hi, I'm Alex.</h1>
-            <div className="text-center lg:text-left text-xl md:text-2xl mt-8">
-                <Typewriter
-                    options={{
-                        strings: [
-                            'Senior Software Engineer',
-                            'AWS Certified Soluctions Architect'
-                        ],
-                        autoStart: true,
-                        loop: true,
-                        delay: 50
-                    }}
-                />
+        <section className="lg:flex lg:items-center justify-evenly mt-4">
+            <div className="w-auto text-center lg:text-left">
+                <h1 className="text-6xl">Hi, I'm Alex.</h1>
+                <div className="text-xl md:text-2xl mt-8">
+                    <Typewriter
+                        options={{
+                            strings: [
+                                'Senior Software Engineer',
+                                'AWS Certified Soluctions Architect'
+                            ],
+                            autoStart: true,
+                            loop: true,
+                            delay: 50
+                        }}
+                    />
+                </div>
             </div>
+            <img className="hidden lg:flex" src={me} alt="" />
         </section>
     );
 }
