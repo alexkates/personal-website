6b74f51f843816f87f2fb1fe41deb7ef0f87a768
Daniel Alexander Kates <alex.kates@creditly.com>
2021-07-03 17:31:06 -0400

    initial commit after npx create-next-app

--- src/Tweet.js
+++
@@ -1,9 +0,0 @@
-import { Tweet as TweetWidget } from 'react-twitter-widgets'
-
-export default function Tweet({ tweetId }) {
-  return (
-    <div className="px-4 py-8 lg:p-8">
-      <TweetWidget tweetId={tweetId} options={{ theme: "dark" }} />
-    </div>
-  )
-}
